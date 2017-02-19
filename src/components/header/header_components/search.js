95e5f4894e40a2e07ed06d42b715f9d976510d70
Ivan <Holy.vinil@gmail.com>
2017-02-19 16:09:52 +0200

    search component

--- src/components/header/header_components/search.js
+++ src/components/header/header_components/search.js
@@ -16,11 +16,20 @@ const styles = {
         position: "absolute",
         bottom: 17,
         right: 35
-
     }
 };
 
 class Search extends Component {
+    constructor(props){
+        super(props)
+        this.displaySearch = this.displaySearch.bind(this)  
+    }
+    displaySearch = (event)=> {
+        var search = document.getElementById("search");
+        var style = window.getComputedStyle(search,"");
+        search.style.display = (style.display == 'none')?'block':'none';
+    };
+
     render() {
         return (
             <div>
@@ -29,12 +38,13 @@ class Search extends Component {
                             hoveredStyle={styles.hoveredStyle}
                             style={styles.styleButton}
                             className="search"
+                            onClick={this.displaySearch}
                 >
                     <FontIcon className="material-icons">search</FontIcon>
                 </IconButton>
 
-                <form>
-                        <input id="search" type="search" required/>
+                <form onSubmit={this.handleSubmit}>
+                    <input id="search" type="search" placeholder="Search for something..." required/>
                 </form>
             </div>
         )
