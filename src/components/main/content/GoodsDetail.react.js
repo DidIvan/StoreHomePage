a7ae68867d9d580262043d9471b969b957f0d437
Ivan <Holy.vinil@gmail.com>
2017-02-23 23:04:51 +0200

    add component goodsDetail

---
+++ src/components/main/content/GoodsDetail.react.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+import '../../../css/components-style/goodsDetail.css';
+const DEFAULT_PICTURE1 = '../../../../public/img/default/no_photo.png';
+
+var pictureConverter1 = function (picture) {
+    if (picture) {
+        return picture;
+    }
+    return DEFAULT_PICTURE1;
+}
+
+var goodsDetail = {
+    "id": '1',
+    "title": 'Pump',
+    "picture": 'img/default/no_photo.png',
+    "description": 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cum cumque' +
+    ' deserunt enim exercitationem explicabo minima molestiae nam neque nisi perspiciatis praesentium,' +
+    ' quae quos similique sint sit sunt tempore.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cum cumque' +
+    ' deserunt enim exercitationem explicabo minima molestiae nam neque nisi perspiciatis praesentium,' +
+    ' quae quos similique sint sit sunt tempore.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cum cumque' +
+    ' deserunt enim exercitationem explicabo minima molestiae nam neque nisi perspiciatis praesentium,' +
+    ' quae quos similique sint sit sunt tempore.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cum cumque' +
+    ' deserunt enim exercitationem explicabo minima molestiae nam neque nisi perspiciatis praesentium,' +
+    ' quae quos similique sint sit sunt tempore.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores consectetur cum cumque' +
+    ' deserunt enim exercitationem explicabo minima molestiae nam neque nisi perspiciatis praesentium,' +
+    ' quae quos similique sint sit sunt tempore.',
+    "price": '150',
+    "propertyValues": [
+        {
+            "id": '1',
+            "name": "supply",
+            "value": 220,
+            "measureName": 'volts',
+            "measureCon": 'VAC'
+        }, {
+            "id": '2',
+            "name": "pressure",
+            "value": 10,
+            "measureName": 'bar',
+            "measureCon": 'bar'
+        }, {
+            "id": '3',
+            "name": "power",
+            "value": 0.06,
+            "measureName": 'Watt',
+            "measureCon": 'kW'
+        }
+    ]
+}
+
+class GoodsDetail extends Component {
+    render() {
+        var properties = goodsDetail.map((item)=> {
+                return (
+                    <li>{item.name} : {item.value} {item.measureCon}</li>
+                );
+            }
+        );
+        return (
+            <div className="goods-detail">
+                <div className="row">
+                    <div className="col s12">
+                        <div className="card-panel">
+
+                            <div className="title-detail">
+                                <h5 className="truncate">{goodsDetail.title}</h5>
+                            </div>
+
+                            <div className="row">
+                                <div className="card-image left">
+                                    <a href="#">
+                                        <img src={pictureConverter1(goodsDetail.picture)}/></a>
+                                </div>
+                                <div className="description">
+                                    {goodsDetail.description}
+                                </div>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="row">
+                                <div className="rating">
+                                </div>
+                                <a className="waves-effect waves-light btn green right">Заказать</a>
+                            </div>
+                            <div className="divider"></div>
+                            <div className="row">
+                                <div className="detail-property left">
+                                    <h5>Характеристики</h5>
+                                    <ul>{properties}</ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+}
+;
+
+export default GoodsDetail;
--- src/components/main/main.react.js
+++ src/components/main/main.react.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import Goods from './content/goods.react';
+import GoodsDetail from './content/GoodsDetail.react';
 
 import '../../css/components-style/style.css';
 import '../../css/components-style/main.css';
 
 var Main = React.createClass({
-
     render() {
         return (
             <div>
@@ -20,14 +20,15 @@ var Main = React.createClass({
                         <li><a href="#">accessories</a></li>
                     </ul>
                     <div className="main_cards">
-                        <Goods image="../../img/content/Base1.jpg" price="$23" goodName="Printed swimsuit"/>
+                        {  /*          <Goods image="./img/content/Base1.jpg" price="$23" goodName="Printed swimsuit"/>
                         <Goods image="../../img/content/Base2.jpg" oldprice="$32" newprice="$12" goodName="Jogging bermuda shorts"/>
                         <Goods image="../../img/content/Base3.jpg" price="$9" goodName="Basic beach flip flops"/>
                         <Goods image="../../img/content/Base4.jpg" price="$29" goodName="Black sunglasses"/>
                         <Goods image="../../img/content/Base5.jpg" price="$20" goodName="Long sleeved twill shirt"/>
                         <Goods image="../../img/content/Base6.jpg" price="$36" goodName="Basic swimsuit"/>
                         <Goods image="../../img/content/Base7.jpg" price="$12" goodName="Bleached shorts"/>
-                        <Goods image="../../img/content/Base8.jpg" oldprice="$32" newprice="$12" goodName="T-shirt with lower panel"/>
+                        <Goods image="../../img/content/Base8.jpg" oldprice="$32" newprice="$12" goodName="T-shirt with lower panel"/>*/}
+                        <GoodsDetail/>
                     </div>
                 </main>
             </div>
